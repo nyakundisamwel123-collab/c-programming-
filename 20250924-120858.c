4e8ec3059b7e7c898eb4db3fb5f8a14f52accb44
nyakundisamwel123-collab <nyakundisamwel123@gmail.com>
2025-09-25 02:37:01 -0600

    Add files via upload
    Initial commit of variables c

---
+++ 20250924-120858.c
@@ -0,0 +1,19 @@
+//program outlining the units for semester 1
+
+/*
+Samwel Nyakundi 
+PA106/G/28824/25
+program to display units for semester 1 year 1
+*/
+
+#include<stdio.h> // preprocessor directive 
+// main function 
+int main() {
+printf("1. Introduction to programming and problem solving\n");
+printf("2. Computer systems and organisation\n");
+printf("3. Discrete mathematics\n");
+printf("4. Mathematics for science\n");
+printf("5. Fundamentals of system analysis and design\n");
+printf("6. Fundamentals of ICT\n");
+printf("7. Communication and information literacy skills\n");
+}
---
+++ 20250925-112153.c
@@ -0,0 +1,22 @@
+//simple program to prompt user to enter ID number,height and bank balance 
+/*
+Samwel Nyakundi
+PA106/G/28824/25
+program to prompt user to enter ID number height and bank balance 
+*/
+#include<stdio.h>
+//main function 
+int main (){
+
+int id;
+int height;
+int bank_balance;
+
+
+printf("enter your ID number");
+scanf("%d & height");
+
+printf("enter your bank balance");
+scanf("%d, & bank balance");
+
+}
